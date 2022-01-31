321165c336865aa5f51637cb245d9d3e45b0d9a7
sararekowska <sara.rekowska@gmail.com>
2022-01-31 21:28:45 +0100

    style: character page

--- src/components/CharPage/CharPage.tsx
+++ src/components/CharPage/CharPage.tsx
@@ -3,6 +3,7 @@ import { Link, useParams } from "react-router-dom";
 import commonstyles from "../../common/Styles.module.scss";
 import { useCharacter } from "../../common/hooks/useCharacter";
 import { CharImage } from "../CharList/CharList";
+import styles from "./CharPage.module.scss";
 
 const CharPage = () => {
   const { name } = useParams<{ name: string }>();
@@ -17,15 +18,20 @@ const CharPage = () => {
           className={commonstyles["back-icon"]}
         />
       </Link>
-      <main>
-        <h1>{data.name}</h1>
-        <h2>{data.vision}</h2>
-        <h2>Rarity: {data.rarity}</h2>
-        <CharImage char={data} img="/portrait" />
-        <p>Nation: {data.nation}</p>
-        <p>Weapon: {data.weapon}</p>
-        <p>{data.description}</p>
-      </main>
+      <div className={styles["main-char"]}>
+        <div className={styles["left-desc"]}>
+          <p>{data.name}</p>
+          <CharImage char={data} img="/portrait" />
+        </div>
+
+        <div className={styles["right-desc"]}>
+          <p>Vision: {data.vision}</p>
+          <p>Rarity: {data.rarity}</p>
+          <p>Nation: {data.nation}</p>
+          <p>Weapon: {data.weapon}</p>
+          <p>{data.description}</p>
+        </div>
+      </div>
     </>
   ) : null;
 };
